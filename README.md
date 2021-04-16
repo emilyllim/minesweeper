029e589fc85c0303d7cade26edfad4e199c74d6f
emily.lim <emily.lim@wsu.edu>
2021-04-16 12:10:31 -0700

    Grid and score layout added with 100 buttons. Button clicked function and README file added.

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 224 Minesweeper
+## Emily Lim
+
+To run:
+python3 main.py
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from PyQt5.QtWidgets import QApplication
 from MineWindow import *
+
+app = QApplication([])
+window = MineWindow()
+window.show()
+app.exec_()
