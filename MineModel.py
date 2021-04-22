9ebbb87787dfe47c5d706a9cdd89ac3416bf638f
emily.lim <emily.lim@wsu.edu>
2021-04-21 21:00:03 -0700

    Adjacent bomb counts added into 2d array with for loops and if statements.

--- MineModel.py
+++ MineModel.py
@@ -10,38 +10,130 @@ class MineModel:
 		cols = 10
 		# Each button is set 0, meaning they haven't been clicked yet
 		self.grid = [[0]*(cols) for i in range(rows)]
+		# Bombs in the board are represented as an X
+		bomb = 'X'
 		# Counts number of bombs added to the board
 		bombNum = 0
+		# Stores bombs adjacent to current button
+		adj = 0
 
 		while bombNum != 10:
 			# Randomly add bombs to certain buttons
 			# For location for one bomb:
 			i = random.randint(0, rows-1)
 			j = random.randint(0, cols-1)
-			if self.grid[i][j] != 'X':
-				self.grid[i][j] = 'X'
+			if self.grid[i][j] != bomb:
+				self.grid[i][j] = bomb
 				bombNum += 1
-				print(f"Random row {i} and random col {j}")
+				#print(f"Random row {i} and random col {j}")
 
 
-		for row in range(len(self.grid)):
-			for col in range(len(self.grid)):
+		for row in range(rows):
+			for col in range(cols):
 				#print(f"Row {row} and col {col}")
-				print(f"{self.grid[row][col]}")
-				# if grid[row][col+1] == 'X':
-				# 	grid[row][col] += 1
+				if self.grid[row][col] != bomb:
+					if col == 0:
+						if row == 0: # First row
+							if self.grid[row][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col] == bomb:
+								adj+=1
+						elif row == rows-1: # Last row
+							if self.grid[row-1][col] == bomb:
+								adj+=1
+							if self.grid[row-1][col+1] == bomb:
+								adj+=1
+							if self.grid[row][col+1] == bomb:
+								adj+=1
+						else: # Rows in between
+							if self.grid[row-1][col] == bomb:
+								adj+=1
+							if self.grid[row-1][col+1] == bomb:
+								adj+=1
+							if self.grid[row][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col] == bomb:
+								adj+=1
+					elif col == rows-1:
+						if row == 0:
+							if self.grid[row+1][col] == bomb:
+								adj+=1
+							if self.grid[row+1][col-1] == bomb:
+								adj+=1
+							if self.grid[row][col-1] == bomb:
+								adj+=1
+						elif row == rows-1:
+							if self.grid[row-1][col-1] == bomb:
+								adj+=1
+							if self.grid[row-1][col] == bomb:
+								adj+=1
+							if self.grid[row][col-1] == bomb:
+								adj+=1
+						else:
+							if self.grid[row-1][col-1] == bomb:
+								adj+=1
+							if self.grid[row-1][col] == bomb:
+								adj+=1
+							if self.grid[row+1][col] == bomb:
+								adj+=1
+							if self.grid[row+1][col-1] == bomb:
+								adj+=1
+							if self.grid[row][col-1] == bomb:
+								adj+=1
+					else:
+						if row == 0:
+							if self.grid[row][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col] == bomb:
+								adj+=1
+							if self.grid[row+1][col-1] == bomb:
+								adj+=1
+							if self.grid[row][col-1] == bomb:
+								adj+=1
+						elif row == rows-1:
+							if self.grid[row-1][row-1] == bomb:
+								adj+=1
+							if self.grid[row-1][col] == bomb:
+								adj+=1
+							if self.grid[row-1][col+1] == bomb:
+								adj+=1
+							if self.grid[row][col+1] == bomb:
+								adj+=1
+							if self.grid[row][col-1] == bomb:
+								adj+=1
+						else:
+							if self.grid[row-1][col-1] == bomb:
+								adj+=1
+							if self.grid[row-1][col] == bomb:
+								adj+=1
+							if self.grid[row-1][col+1] == bomb:
+								adj+=1
+							if self.grid[row][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col+1] == bomb:
+								adj+=1
+							if self.grid[row+1][col] == bomb:
+								adj+=1
+							if self.grid[row+1][col-1] == bomb:
+								adj+=1
+							if self.grid[row][col-1] == bomb:
+								adj+=1
 
-		# Add number of bombs around a certain button
+
+					self.grid[row][col] += adj
+					adj = 0
 
 
 
-
+	# Returns what's in the button clicked
 	def reveal(self, row, col):
-		# Reveal what's in the button clicked
-
 		return self.grid[row][col]
-
-
 
 
 	# def getSquare():
