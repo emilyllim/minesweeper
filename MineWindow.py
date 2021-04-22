d6eb56b5f8f04df20c80c4d094285908147ced98
emily.lim <emily.lim@wsu.edu>
2021-04-21 23:12:52 -0700

    Added move counter. Added flag button that when green can flag buttons on grid.

--- MineWindow.py
+++ MineWindow.py
@@ -16,10 +16,29 @@ class MineWindow(QMainWindow):
 		layoutV = QVBoxLayout()
 		# Add a QHBoxLayout with a QLabel
 		layoutH = QHBoxLayout()
-		self.label = QLabel("Score")
+		self.label = QLabel("Bombs: 10")
 		font = QFont("Arial", 30, QFont.Bold)
 		self.label.setFont(font)
 		layoutH.addWidget(self.label)
+
+		# Holds count of number of buttons clicked
+		self.clickCount = 0
+		# Label showing number of moves made
+		self.moveLabel = QLabel("Moves made: " + str(self.clickCount))
+		self.moveLabel.setFont(font)
+		layoutH.addWidget(self.moveLabel)
+
+		# Button to press if player wants to flag buttons
+		self.flagButton = QPushButton()
+		self.flagButton.setText("Flag")
+		font = QFont("Arial", 20, QFont.Bold)
+		self.flagButton.setFont(font)
+		self.flagButton.setStyleSheet("background-color : light gray")
+		self.flagButton.setProperty("on", False)
+		self.flagButton.setFixedSize(QSize(100, 50))
+		self.flagButton.clicked.connect(self.flagClicked)
+		layoutH.addWidget(self.flagButton)
+
 		# Add it to the vertical layout
 		layoutV.addLayout(layoutH)
 		# Set widget to the vertical layout
@@ -29,16 +48,15 @@ class MineWindow(QMainWindow):
 
 		# Holds references to the buttons
 		self.buttons = []
-		# Holds count of number of buttons clicked
-		self.clickCount = 0
 
 		# 100 buttons are created
 		for i in range(100):
 			button = QPushButton()
 			button.setText(" ")
-			font = QFont("Arial", 30, QFont.Bold)
+			font = QFont("Arial", 35, QFont.Bold)
 			button.setFont(font)
 			button.setStyleSheet("background-color : light gray")
+			button.setFixedSize(QSize(100, 100))
 			# Tells which button was clicked
 			button.clicked.connect(self.buttonClicked)
 			# These buttons are added to the buttons list
@@ -51,6 +69,7 @@ class MineWindow(QMainWindow):
 			button.setProperty("myCol", col)
 			# Each button is added to the grid
 			gridLayout.addWidget(self.buttons[i], row, col)
+
 		# Add grid to the vertical layout
 		layoutV.addLayout(gridLayout)
 
@@ -70,12 +89,21 @@ class MineWindow(QMainWindow):
 
 
 	def newGame(self):
-		print("New game!");
-		# Set all buttons to true
+		# Reset all buttons
 		for button in self.buttons:
 			button.setEnabled(True)
 			button.setText(" ")
 			button.setStyleSheet("background-color : light gray")
+		# Reset flag button
+		self.flagButton.setEnabled(True)
+		self.flagButton.setStyleSheet("background-color : light gray")
+		self.flagButton.setProperty("on", False)
+		# Reset buttons clicked to 0
+		self.clickCount = 0
+		# Set bomb label to 10
+		self.label.setText("Bombs: 10")
+		# Reset moves label
+		self.moveLabel.setText("Moves made: " + str(self.clickCount))
 		# Model generates a new game
 		self.model.newGame()
 
@@ -84,28 +112,42 @@ class MineWindow(QMainWindow):
 		# Gets each button was clicked
 		clicked = self.sender()
 		self.clickCount += 1
+		self.moveLabel.setText("Moves made: " + str(self.clickCount))
 		# Row and col and equal to the row and col button clicked is in
 		row = clicked.property("myRow")
 		col = clicked.property("myCol")
 
-		square = str(self.model.getSquare(row, col))
-		if square == 'X':
-			clicked.setStyleSheet("background-color : red")
+		if self.flagButton.property("on") == False:
+			square = str(self.model.getSquare(row, col))
+			if square == 'X':
+				clicked.setStyleSheet("background-color : red")
+			else:
+				clicked.setStyleSheet("background-color : blue")
+			# Button clicked now shows a bomb or how many bombs are adjacent
+			clicked.setText(square)
+
+			# If a button with a bomb is clicked, game is over
+			if self.model.getGameState(self.clickCount, row, col) == 0:
+				self.label.setText("Game Over!")
+				for button in self.buttons:
+					button.setEnabled(False)
+
+			# If all buttons have been clicked, game is won
+			if self.model.getGameState(self.clickCount, row, col) == 1:
+				self.label.setText("You Won!")
+
 		else:
-			clicked.setStyleSheet("background-color : blue")
-
-		if self.model.getGameState(self.clickCount, row, col) == 0:
-			print("You lost!")
-
-		if self.model.getGameState(self.clickCount, row, col) == 1:
-			print("You won!")
-
-
-		# Button clicked now shows a bomb or how many bombs are adjacent
-		clicked.setText(square)
+			clicked.setStyleSheet("background-color : green")
 
 		# Button can't be clicked anymore
 		clicked.setEnabled(False)
 
-		# Tell model of which button was clicked
-		# self.model.reveal(row, col)
+	def flagClicked(self):
+		clicked = self.sender()
+
+		if clicked.property("on") == False:
+			self.flagButton.setStyleSheet("background-color : green")
+			self.flagButton.setProperty("on", True)
+		else:
+			self.flagButton.setStyleSheet("background-color : light gray")
+			self.flagButton.setProperty("on", False)
